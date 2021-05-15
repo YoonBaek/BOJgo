e06d32c09240881c63c5e2227974d8bb9c3e7e7b
YoonBaek <royal729@naver.com>
2021-05-16 08:54:02 +0900

    solved 15650

---
+++ BackTracking/NandM/15649/15649N과M1.go
@@ -0,0 +1,42 @@
+// Solved by Github YoonBaek
+
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"os"
+)
+
+var (
+	n, m    int
+	visited []bool
+	arr     []int
+	rd      = bufio.NewReader(os.Stdin)
+	wr      = bufio.NewWriter(os.Stdout)
+)
+
+func dfs(cnt int) {
+	if cnt == m {
+		for i := 0; i < m; i++ {
+			Fprintf(wr, "%d ", arr[i])
+		}
+		Fprintln(wr)
+	}
+	for i := 0; i < n; i++ {
+		if !visited[i] {
+			visited[i] = true
+			arr[cnt] = i + 1
+			dfs(cnt + 1)
+			visited[i] = false
+		}
+	}
+}
+
+func main() {
+	defer wr.Flush()
+	Fscanf(rd, "%d %d\n", &n, &m)
+	visited = make([]bool, n)
+	arr = make([]int, n)
+	dfs(0)
+}
