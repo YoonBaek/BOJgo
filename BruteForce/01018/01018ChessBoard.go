6c1ea0d92701ca8b26cbaa0361ececd0c113acb0
YoonBaek <royal729@naver.com>
2021-05-28 18:36:52 +0900

    solved 1018 BruteForce

---
+++ BruteForce/01018/01018ChessBoard.go
@@ -0,0 +1,66 @@
+// Solved by Github YoonBaek
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	wr := bufio.NewWriter(os.Stdout)
+	defer wr.Flush()
+	var n, m, cntB, cntW int
+	min := int(math.Inf(1))
+
+	if sc.Scan() {
+		input := sc.Text()
+		inputs := strings.Split(input, " ")
+		n, _ = strconv.Atoi(inputs[0])
+		m, _ = strconv.Atoi(inputs[1])
+	}
+
+	Board := make([][]rune, n)
+
+	for i := 0; i < n; i++ {
+		if sc.Scan() {
+			inputs := sc.Text()
+			Board[i] = []rune(inputs)
+		}
+	}
+
+	for i := 0; i < n-7; i++ {
+		for j := 0; j < m-7; j++ {
+			cntB, cntW = 0, 0
+			for k := 0; k < 8; k++ {
+				for l := 0; l < 8; l++ {
+					if (k+l)%2 == 0 {
+						if Board[i+k][j+l] == 'B' {
+							cntW++
+						} else {
+							cntB++
+						}
+					} else {
+						if Board[i+k][j+l] == 'B' {
+							cntB++
+						} else {
+							cntW++
+						}
+					}
+				}
+			}
+			if cntW < min {
+				min = cntW
+			}
+			if cntB < min {
+				min = cntB
+			}
+		}
+	}
+	fmt.Fprintln(wr, min)
+}
